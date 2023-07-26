b17621450b473d51a7e54ba90695e237657e496f
Karan Bhati <bhatikaran467@gmail.com>
2023-07-26 16:50:37 +0530

    changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://localhost:3000',
+    siteUrl: 'https://randomposts.netlify.app',
     generateRobotsTxt: true, 
   }
